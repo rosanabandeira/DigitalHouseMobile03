bc94ccef56c1dea2b65c03608d3056023da4310e
Rosana <rosanaflag@gmail.com>
2018-09-06 14:00:42 -0300

    licao de casa 2

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/RegisterPhotoctivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/RegisterPhotoctivity.java
@@ -2,21 +2,12 @@ package br.com.digitalhouse.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterPhotoctivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
-        
-
-
-
-
+        setContentView(R.layout.activity_register_photoctivity);
     }
 }
