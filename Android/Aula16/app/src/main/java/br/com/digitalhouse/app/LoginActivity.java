0afe12897c7a9824710a275bc8439c84206b9038
Rosana <rosanaflag@gmail.com>
2018-09-10 21:48:58 -0300

    setonclick

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
@@ -1,15 +1,29 @@
 package br.com.digitalhouse.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView registerNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        registerNow = (TextView) findViewById(R.id.textView_register);
+
+        registerNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
 
 
     }
