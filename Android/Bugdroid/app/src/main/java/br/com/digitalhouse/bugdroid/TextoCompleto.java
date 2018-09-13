5c70e13a430e30a6807d560c2b30a9f619f49709
Rosana <rosanaflag@gmail.com>
2018-09-12 22:22:45 -0300

    tarefas

---
+++ Android/Bugdroid/app/src/main/java/br/com/digitalhouse/bugdroid/TextoCompleto.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bugdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextoCompleto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texto_completo);
+    }
+}
---
+++ Android/Bugdroid/app/src/main/java/br/com/digitalhouse/bugdroid/TextoResumo.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.bugdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextoResumo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texto_resumo);
+    }
+}
